872276d5cf4dfc9a46a89e9c52198cf6104a8268
haris-naveed <harisnaveed1997@gmail.com>
2022-12-08 20:51:11 +0500

    first

---
+++ food-proj/src/App.js
@@ -0,0 +1,57 @@
+
+import './App.css';
+import React, { useEffect,useState } from 'react';
+import {useDispatch ,useSelector } from 'react-redux'
+import getAllPizzas from './actions/pizzaAction'
+import Header from './components/HEADER/Header';
+import Meals  from './components/MEAL-ITEMS/Meals';
+import Cart from './components/CART/Cart';
+
+
+import CartProvider from './store/CartProvider';
+import SignIn from './components/signup/signIn';
+import { Auth } from './components/AUTH/Auth';
+
+
+function App() {
+  
+  const dispatch=useDispatch();
+  const user=useSelector((state)=>state?.AuthReducer?.authData);
+  const [profile,setProfile]=useState(JSON.parse(localStorage.getItem('user')));
+
+ 
+  useEffect(()=>{
+    dispatch(getAllPizzas())
+    if(!user)
+    setProfile(null)
+    setProfile(JSON.parse(localStorage.getItem('user')))
+    console.log(user,profile)
+
+  },[user,profile]);
+
+  
+  const [cartIsShown,setCartIsShown]=useState(true);
+
+
+  const showCartHandler=()=>{
+    setCartIsShown(true);
+
+  }
+  const HideCartHandler=()=>{
+    setCartIsShown(false);
+  }
+  return (
+<CartProvider>
+  {/* <SignIn/> */}
+  {/* {cartIsShown && <Cart onClose={HideCartHandler}/>} */}
+<Header onShowCart={showCartHandler}/>
+{
+  profile?.result?<Meals/>:<Auth/>
+}
+  {/* <Auth/>
+<Meals/> */}
+</CartProvider>
+  );
+}
+
+export default App;
